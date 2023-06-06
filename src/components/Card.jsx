60ab35e46c0d2ca526715a5b68660746016cc85e
Sahil R <sahilrw000@gmail.com>
2023-06-06 13:59:47 +0530

    finished styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -23,18 +23,18 @@ const Card = ({ movie }) => {
                     <Link to={`/movie/${movie.id}`}>
                         <div className='inline-block relative overflow-hidden cursor-pointer z-0 border-2'>
                             <img className="h-80" alt={movie.original_title} src={`https://image.tmdb.org/t/p/original${movie ? movie.poster_path : ""}`} />
-                            <div className='bg-[linear-gradient(rgb(0,0,0,0), rgb(0,0,0,1)] absolute bottom-0 w-10/12 h-24 flex flex-col justify-end opacity-0 transition-opacity duration-200 hover:opacity-100'>
-                                <h2>{movie ? movie.original_title : ''}</h2>
-                                <div className='text-red-600'>
+                            <div className='px-2 pb-1 bg-[linear-gradient(rgb(0,0,0,0), rgb(0,0,0,1)] absolute bottom-0 w-10/12 h-24 flex flex-col justify-end opacity-0 transition-opacity duration-200 hover:opacity-100'>
+                                <h2 className='text-xl font-bold'>{movie ? movie.original_title : ''}</h2>
+                                <div className='text-red-600 font-semibold'>
                                     {movie ? movie.release_date : ''}
-                                    <span>
+                                    <span className='px-2'>
                                         {movie ? movie.vote_average : ''}
                                         <i className='fas fa-star' />
                                     </span>
                                 </div>
-                                <div>
+                                <p className='italic'>
                                     {movie ? movie.overview.slice(0, 120) + "..." : ''}
-                                </div>
+                                </p>
                             </div>
                         </div>
                     </Link>
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -17,23 +17,27 @@ const MovieDetails = () => {
     }
 
     return (
-        <section>
-            <div>
-                <img src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`} alt="" />
+        <section className='mt-28 mx-10 flex lg:flex-row-reverse flex-col mb-60'>
+            <div className='px-4 my-2'>
+                <img src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`} alt="movie.title" />
+                <h2 className='text-2xl font-medium'>{currentMovieDetail ? currentMovieDetail.original_title : ''}</h2>
+                <p className='text-xl font-medium italics'>Tagline: {currentMovieDetail ? currentMovieDetail.tagline : ''}</p>
             </div>
-            <section>
-                <div>
-                    <h3>Synopsis</h3>
-                    <p>{currentMovieDetail ? currentMovieDetail.overview : ""}</p>
+            <section className='lg:w-2/3'>
+                <div className='pb-2'>
+                    <h3 className='text-2xl text-center py-1 border-2 border-solid border-gray-100'>Synopsis</h3>
+                    <p className='mt-2 italic'>{currentMovieDetail ? currentMovieDetail.overview : ""}</p>
+                </div>
+                <div className='mt-6'>
+                    <h3 className='text-2xl text-center py-1 border-2 border-solid border-gray-100 mb-1'>Useful Links</h3>
+                    <div className='flex flex-col'>
+                        {currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" rel="noreferrer"><p><span>Homepage <i className="fas fa-external-link-alt"></i></span></p></a>}
+                        {
+                            currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" rel="noreferrer"><p><span>IMDb<i className="fas fa-external-link-alt"></i></span></p></a>
+                        }
+                    </div>
                 </div>
             </section>
-            <div>
-                <h3>Useful Links</h3>
-                {currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" rel="noreferrer"><p><span>Homepage <i className="fas fa-external-link-alt"></i></span></p></a>}
-                {
-                    currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" rel="noreferrer"><p><span>IMDb<i className="fas fa-external-link-alt"></i></span></p></a>
-                }
-            </div>
         </section>
     )
 }
