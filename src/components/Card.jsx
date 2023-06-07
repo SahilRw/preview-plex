c0bda7d64964e7df26935d2480e5434d30083726
Sahil R <sahilrw000@gmail.com>
2023-06-07 18:45:29 +0530

    loading state updates...

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,16 +14,17 @@ const Card = ({ movie }) => {
             {
                 isLoading
                     ?
-                    <div>
-                        <SkeletonTheme>
-                            <Skeleton height={300} />
+                    <div className='bg-[#202020] inline-block relative transition-transform duration-200 rounded-xl overflow-hidden m-1 cursor-pointer z-0 hover:z-50 min-w-[200px] h-72 border-2 border-solid border-white'>
+                        <SkeletonTheme highlightColor="#444">
+                            <Skeleton height={300} duration={2} />
                         </SkeletonTheme>
                     </div>
                     :
                     <Link to={`/movie/${movie.id}`}>
-                        <div className='inline-block relative overflow-hidden cursor-pointer z-0 border-2'>
+                        {/* <div className='inline-block relative overflow-hidden cursor-pointer z-0 border-2'> */}
+                        <div className='inline-block relative transition-transform duration-200 rounded-xl overflow-hidden m-1 cursor-pointer z-0 hover:z-50 min-w-[200px] h-72 border-2 border-solid border-white'>
                             <img className="h-80" alt={movie.original_title} src={`https://image.tmdb.org/t/p/original${movie ? movie.poster_path : ""}`} />
-                            <div className='px-2 pb-1 bg-[linear-gradient(rgb(0,0,0,0), rgb(0,0,0,1)] absolute bottom-0 w-10/12 h-24 flex flex-col justify-end opacity-0 transition-opacity duration-200 hover:opacity-100'>
+                            <div className=' px-2 pb-1 bg-[linear-gradient(rgb(0,0,0,0), rgb(0,0,0,1)] absolute bottom-0 w-10/12 h-24 flex flex-col justify-end opacity-0 transition-opacity duration-200 hover:opacity-100'>
                                 <h2 className='text-xl font-bold'>{movie ? movie.original_title : ''}</h2>
                                 <div className='text-red-600 font-semibold'>
                                     {movie ? movie.release_date : ''}
