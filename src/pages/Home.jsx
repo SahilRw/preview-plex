66ab4e013ee5b020c733477752320c7894ef2dda
Sahil R <sahilrw000@gmail.com>
2023-06-10 14:50:29 +0530

    nav changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,6 @@ const Home = () => {
     const [popularMovies, setPopularMovies] = useState([])
 
     useEffect(() => {
-        // fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=737e70d53383213111fd262835188b45&language=en-US`)
             .then(res => res.json())
             .then(data => setPopularMovies(data.results))
@@ -30,10 +29,19 @@ const Home = () => {
                     popularMovies.map(movie => (
                         <Link key={movie.id} to={`/movie/${movie.id}`}>
                             <div className='lg:h-3/4 lg:w-3/4 mx-auto'>
-                                <img className='' src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} alt="movie.original_title" />
+                                <img
+                                    src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`}
+                                    alt="movie.original_title"
+                                />
                             </div>
-                            <div className='flex flex-col lg:items-center text-left absolute bottom-0 w-full lg:w-1/3 px-4 pb-8 opacity-0 hover:opacity-100 lg:inset-x-1/3 lg:text-center'>
-                                <h2 className='text-4xl font-bold'>{movie ? movie.original_title : ''}</h2>
+                            <div className='flex flex-col lg:items-center 
+                                            text-left absolute bottom-0 
+                                            w-full lg:w-1/3 px-4 pb-8
+                                            opacity-0 hover:opacity-100 
+                                            lg:inset-x-1/3 lg:text-center'>
+                                <h2 className='text-4xl font-bold'>
+                                    {movie ? movie.original_title : ''}
+                                </h2>
                                 <div className='text-red-600'>
                                     {movie ? movie.release_date : ''}
                                     <span className='px-2'>
